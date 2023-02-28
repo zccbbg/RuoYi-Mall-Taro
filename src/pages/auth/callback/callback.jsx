bbc0ea773f8b013d512bd194e4383fd27fbf8c9c
feijinping <feijinping@foxmail.com>
2023-02-28 19:10:12 +0800

    微信登陆修改

--- src/pages/auth/callback/callback.jsx
+++ src/pages/auth/callback/callback.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Taro from '@tarojs/taro';
-import {Button, View} from '@tarojs/components';
+import {View} from '@tarojs/components';
 
 import './index.less';
 import {wechatH5Login} from "../../../services/user";
@@ -34,14 +34,8 @@ class Callback extends Component {
 
   render() {
     return (
-      <View className='login-wrapper'>
-        <View className='login-box'>
-          {
-            Taro.getEnv() === Taro.ENV_TYPE.WEAPP &&
-            <Button type='primary' openType='getUserInfo' className='wx-login-btn' onGetUserInfo={this.wxLogin}>微信直接登录</Button>
-          }
-          <Button type='primary' className='account-login-btn' onClick={this.accountLogin}>账号登录</Button>
-        </View>
+      <View className='callback-wrapper'>
+        登陆中。。。
       </View>
     );
   }
